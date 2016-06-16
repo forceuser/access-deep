bf8e4b91479f031cc3e582430428e5d2cb297f91
ForceUser <forceuserz@gmail.com>
2016-06-16 12:37:54 +0300

    general test

--- test/index.js
+++ test/index.js
@@ -1,2 +1,9 @@
 const accessDeep = require('../index');
 const test = require('ava');
+
+test('general test', t => {
+  var o = {};
+  var p = accessDeep(o);
+  p.a.b.c = 12;
+  t.is(JSON.stringify(o), '{a:{b:{c:12}}}');
+});
