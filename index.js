9e1450a998404ddaf0fe53293dc585b2479c2a60
ForceUser <forceuserz@gmail.com>
2016-06-16 13:42:59 +0300

    passing test

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
 module.exports = accessDeep;
 
-accessDeep.REWRITE = 1;
-accessDeep.CONVERT = 2;
-accessDeep.ERROR = 3;
+accessDeep.REWRITE = 0;
+accessDeep.CONVERT = 1;
+accessDeep.ERROR = 2;
 
-function isenum(prop, obj) {// is property enumerable, including inherited props
-  for (var p in obj) {
-    if (p === prop) return true;
-  }
-  return false;
+function isenum(prop, obj) { // is property enumerable, including inherited props
+	for (var p in obj) {
+		if (p === prop) {
+			return true;
+		}
+	}
+	return false;
 }
 
 function toObject(val) { // convert primitive type to object
-  return ({}).valueOf.call(val);
+	return ({}).valueOf.call(val);
 }
 
-function accessDeep(obj, mode) {
-  return accessDeepSub.apply(this, arguments);
-  
-  function accessDeepSub(obj, path) {
-    path = path || [];
-    obj = obj || {};
-    return new Proxy(obj, {
-      get: function (obj, prop) {	
-	var val = obj[prop];
-	
-	if (isenum(prop, obj)) {
-	  val = toObject(val); 
+function accessDeep() {
+	return accessDeepSub.apply(this, arguments);
+
+	function accessDeepSub(obj, path) {
+		path = path || [];
+		obj = obj || {};
+		return new Proxy(obj, {
+			get: function (obj, prop) {
+				var val = obj[prop];
+
+				if (isenum(prop, obj)) {
+					val = toObject(val);
+				} else if (prop in obj) {
+					return val;
+				} else {
+					val = {};
+				}
+
+				path.push({
+					obj: obj,
+					prop: prop
+				});
+				return accessDeepSub(val, path);
+			},
+			set: function (obj, prop, val) {
+				var p;
+				var step = obj;
+
+				obj[prop] = val;
+
+				while (path.length) {
+					p = path.pop();
+					p.obj[p.prop] = step;
+					step = p.obj;
+				}
+				return true;
+			}
+		});
 	}
-	else if (!(prop in obj)) {
-	  val = {};
-	}
-	else {
-	  return val;
-	}
-	
-	path.push({
-	  obj: obj,
-	  prop: prop
-	});
-	return accessDeepSub(val, path);
-      },
-      set: function (obj, prop, val) {
-	var p, step = obj;
-	
-	obj[prop] = val;
-	
-	while (path.length) {
-	  p = path.pop();
-	  p.obj[p.prop] = step;
-	  step = p.obj;
-	}
-      }
-    });
-  }
-  
 }
